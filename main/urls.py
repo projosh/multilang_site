ab3f6b731c2f015b5e4bb5f7b37f96b71051a73c
steve <steve.berenger@gmail.com>
2024-07-09 17:00:57 +0200

    CRUd, requirement, env

--- main/urls.py
+++ main/urls.py
@@ -2,11 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-     path('', views.article_list, name='article_list'),  # List of articles
+    path('', views.article_list, name='article_list'),  # List of articles
     path('delete/<int:article_id>/', views.delete_article, name='delete_article'),  # Delete article
-    path('add/', views.add_article, name='add_article'),  # Add article
+    path('create/new/', views.create_article, name='create_article'),
+    path('article/<int:article_id>/edit/', views.article_update, name='article_update'),
     path('article/<int:article_id>/', views.article_detail, name='article_detail'),  # Article detail
 ]
    
-
-
--- main/views.py
+++ main/views.py
@@ -1,4 +1,5 @@
 
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.utils.translation import gettext as _
 from main.models import Article
@@ -25,16 +26,24 @@ def delete_article(request, article_id):
         return redirect('article_list')
     return render(request, 'main/delete_article.html', {'article': article})
 
-def add_article(request):
+# create article
+def create_article(request):
     if request.method == 'POST':
         form = ArticleForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('article_list')  # Rediriger vers la liste des articles après ajout
+            return redirect('article_list')
     else:
         form = ArticleForm()
+    return render(request, 'main/article_form.html', {'form': form})
 
-    return render(request, 'main/add_article.html', {'form': form})
-
-
-
+def article_update(request, article_id):
+    article = get_object_or_404(Article, id=article_id)
+    if request.method == 'POST':
+        form = ArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            form.save()
+            return redirect('article_list')
+    else:
+        form = ArticleForm(instance=article)
+    return render(request, 'main/article_form.html', {'form': form})
